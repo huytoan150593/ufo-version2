7da22e254c23f6b215409fd6415ca303bfef8399
Ha Thu Nguyen <hathunguyen@HanoMacBook-Air.local>
2022-11-16 09:23:25 +0900

    director is creating

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
+import Director from './components/Director/Director';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <About />
       <Projects />
+      <Director />
     </div>
   );
 }
---
+++ src/components/Director/Director.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./Director.module.css";
+
+const Director = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.arrow}></div>
+            <div className={styles.wrapper}>
+                <div className={styles.image}>
+                    <img src="./assets/director.jpg" alt="director" />
+                </div>
+                <div className={styles.content}>
+                    <div className={styles.title}>
+                        Director - Producer <br /> Luan Minh
+                    </div>
+                    <div className={styles.des}>
+                        Founder of UFO Entertaiment.
+                        <br /> Has a strong passion for art and has been
+                        pursuing art as a career since 2013.
+                        <br />
+                        Has participated in a lot of film, TVC, viral projects
+                        both domestically and internationally such as The Vault
+                        way down, Tracer, Song Lang, Dreamy eyes, This is Us,
+                        Artemis Fowl... by different roles as PM. AD, Prouducer,
+                        camera man... of 2021.
+                        <br /> Always be creative and adaptive to new things to
+                        make great products.
+                    </div>
+                    <div className={styles.sign}>
+                        <img src="./assets/sign.png" alt="signature" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Director;
